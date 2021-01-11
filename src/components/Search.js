a74a79a2bef467e742c07a621c2e9591598cfb24
Olivia Tufte <ortufte@gmail.com>
2021-01-11 13:52:32 -0600

    create search bar, movie state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 const Search = () => {
+
+    const [movie, setMovie] = React.useState('')
+
     return (
-        <h3>Search Movies</h3>
+        <div>
+            <h3>Search Movies</h3>
+            <input 
+            name="search"
+            // style={BarStyling}
+            placeholder="i.e. Terminator"
+            onChange={(e) => setMovie(e.target.value)}
+            />
+            <h1> {movie} </h1>
+        </div>
     )
 }
 
